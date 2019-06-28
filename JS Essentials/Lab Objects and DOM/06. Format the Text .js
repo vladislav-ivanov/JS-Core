6886e36eab46ad6196f0ebab431b709ee0b241db
vladislav-ivanov <vladislav.ivanov90@gmail.com>
2019-06-28 18:07:46 +0200

    new

---
+++ JS Essentials/Lab Objects and DOM/06. Format the Text .js
@@ -0,0 +1,20 @@
+function solve() {
+    let input = document.getElementById('input');
+    let sentences = input.textContent.toString()
+      .split('.')
+      .filter(x => x !== '')
+      .map(x => x += '.');
+    console.log(sentences);
+  
+    let output = document.getElementById('output');
+  
+    for (let i = 0; i < sentences.length; i++) {
+      let p = document.createElement('p');
+      for (let j = 1; j <= 3 && i < sentences.length; j++ , i++){
+        p.textContent += sentences[i];
+      }
+      output.appendChild(p);
+      i--;
+  }
+  
+  }
