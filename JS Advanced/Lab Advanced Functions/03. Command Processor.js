057eacec11b715c94645c585faa6af554f70e6e8
vladislav-ivanov <vladislav.ivanov90@gmail.com>
2019-07-18 20:43:48 +0200

    new

---
+++ JS Advanced/Lab Advanced Functions/03. Command Processor.js
@@ -0,0 +1,17 @@
+function solve () {
+    let data = ''
+    return {
+      append (str) {
+        data += str
+      },
+      removeStart (n) {
+        data = data.slice(n)
+      },
+      removeEnd (n) {
+        data = data.slice(0, data.length - n)
+      },
+      print () {
+        console.log(data)
+      }
+    }
+  }
